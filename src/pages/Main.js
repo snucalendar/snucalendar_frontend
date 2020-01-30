98d78f94eb1329b425b16844c0a6b93358161cca
leegakyeong <alaska7132@naver.com>
2020-01-30 15:20:23 +0900

    :construction: 메인 페이지에서 요일 맞추기 구현 | 코드 리팩토링이 필요함..

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -16,7 +16,6 @@ export const mapStateToProps = (state) => ({
 const today = new Date();
 const currentYear = today.getFullYear();
 const currentMonth = today.getMonth() + 1;
-const firstDay = new Date(currentYear, currentMonth-1, 1).getDay();
 
 export class Main extends Component {
   state = {
@@ -79,7 +78,7 @@ export class Main extends Component {
   }
 
   render() {
-    console.log(document.cookies)
+    const firstDay = new Date(this.state.currentYear, this.state.currentMonth-1, 1).getDay();
     return ( // 아예 Calendar에서 날짜와 이벤트를 받아오는 게 나을 수도...?
       <div style={{'marginTop' : 30}}>
         <h1>Main</h1>

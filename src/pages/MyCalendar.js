eeb022ef939fe0a480a2525b1a3d375a3b81c5cb
leegakyeong <alaska7132@naver.com>
2020-01-30 15:21:49 +0900

    :construction: 마이캘린더  페이지에서 요일 맞추기 구현

--- src/pages/MyCalendar.js
+++ src/pages/MyCalendar.js
@@ -121,11 +121,12 @@ export class Main extends Component {
       const [year, month, day] = event.date.split(':');
       return year == this.state.currentYear && month == this.state.currentMonth;
     });
+    const firstDay = new Date(this.state.currentYear, this.state.currentMonth-1, 1).getDay();
 
     return ( // 아예 Calendar에서 날짜와 이벤트를 받아오는 게 나을 수도...?
       <div style={{'marginTop' : 30}}>
         <h1>Main</h1>
-        <Calendar month={this.state.currentMonth} days={this.props.month_calendar} changeMonth={this.changeMonth} />
+        <Calendar month={this.state.currentMonth} days={this.props.month_calendar} changeMonth={this.changeMonth} firstDay={firstDay} />
         <Tab menu={{ secondary: true, pointing: true }} panes={this.panes} />
       </div>
     );

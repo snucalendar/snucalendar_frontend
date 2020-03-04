2869125f52b0f434920f9c5c951ebe05bafa0158
jangsus1 <min99101511@naver.com>
2020-03-04 19:53:53 +0900

    :lipstick: calendar hover effect 추가

--- src/components/Day.js
+++ src/components/Day.js
@@ -13,6 +13,7 @@ class Day extends Component {
   }
 
   render() {
+    console.log(this.props.events)
     const events = this.props.events
     .filter(e => e.event_type === 'club')
     .map(e => <p class="event">{e.title}</p>);
@@ -22,10 +23,9 @@ class Day extends Component {
     const modal = this.state.isClicked && this.props.events.length ? <DayEventList year={this.props.year} month={this.props.month} date={this.props.date} events={this.props.events} toggleDayEventList={this.toggleDayEventList} /> : null;
     const colStart = this.props.isFirst ? this.props.firstDay+1 : 'auto'
     return (
-      <a style={{gridColumnStart : colStart, width : '150px', height: '160px', border: '0.01px solid #A57E7E'}} onClick={this.toggleDayEventList}>
+      <a style={{gridColumnStart : colStart, width : '150px', height: '160px', border: '0.01px solid #A57E7E', cursor : (this.props.events.length ? 'pointer' : 'default')}} onClick={this.toggleDayEventList}>
         <span style = {{'font-size':'2em'}}><p style={{'text-align':'left', marginLeft: '10px'}}>{this.props.date}</p></span>
-        <p>{events}</p>
-        <p style={{marginTop: '-10px'}}>{events2}</p>
+        <p>{events}{events2}</p>
         {modal}
       </a>
     );
--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import { Icon } from 'semantic-ui-react'
 
 import * as actionCreators from '../store/actions/index';
 import './EventDetail.css'
@@ -7,10 +8,6 @@ import './EventDetail.css'
 export const mapDispatchToProps = (dispatch) => ({
     // getEvent: (id) => dispatch(actionCreators.getEvent(id)),
     participateEvent: (id, type) => dispatch(actionCreators.participateEvent(id, type)),
-});
-
-export const mapStateToProps = (state) => ({
-    event: state.cd.event,
 });
 
 class EventDetail extends Component {
@@ -21,22 +18,28 @@ class EventDetail extends Component {
     participate = (e) => {
         this.props.participateEvent(this.state.event.id, { type: e.target.value })
             .then((res) => {
-                console.log(res); // 이벤트의 좋아요랑 참여 여부를 가져오면 좋을 듯 여기서든 getEvent에서든! 이미 좋아요했으면 버튼에 표시하기
-                // this.setState({
-                //     event: this.props.event,
-                // });
             });
     }
 
     render() {
         return (
                 <div id = 'detail_modal'>
-                    {this.props.event && this.props.event.title}
-                    <button onClick={this.participate} value="interested">관심있음</button>
-                    <button onClick={this.participate} value="participate">참여하기</button>
+                    <h2 id = "title">{this.props.event.title}</h2>
+                    <Icon id = "like_icon" name = "thumbs up" circular/>
+                    <h3 id = 'place'>장소</h3>
+                    <h3 id = 'time'>시간</h3>
+                    <h3 id = 'participants'>참여자</h3>
+                    
+                    <div id = 'participantsList'>
+                        참여자 목록
+                    </div>
+                    <div id = 'button_group'>
+                        <div id = 'left_column' className = 'button_column'><h3 className = "buttons">관심 있음</h3></div>
+                        <div id = 'right_column' className = 'button_column'><h3 className = "buttons">참여하기</h3></div>
+                    </div>
                 </div>
         );
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(EventDetail);
+export default connect(null, mapDispatchToProps)(EventDetail);
